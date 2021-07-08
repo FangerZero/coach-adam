a0c58559ec153fea82437cb5b4ca8f43d6fe3e1b
FangerZero <FangerZero@hotmail.com>
2021-07-07 22:59:03 -0500

    Added navigation file and folder as placeholder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/Link'
 
 function App() {
   return (
@@ -15,9 +16,10 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Today 
         </a>
       </header>
+      <Navigation />
     </div>
   );
 }
---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,13 @@
+function Navigation() {
+    return (
+      <div>
+          Home
+          <br /> 
+          Coach Adam
+          <br /> 
+          Sign up 
+      </div>  
+    );
+}
+
+export default Navigation;

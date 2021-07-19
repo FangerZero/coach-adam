2ca1fdf40c327100cb98693f4ffb9912d43c39f2
FangerZero <FangerZero@hotmail.com>
2021-07-19 00:30:03 -0500

    Added Testimonial Placeholders

---
+++ src/components/testimonials/Testimonials.js
@@ -0,0 +1,20 @@
+import './Testimonials.css';
+// Testimonial will be created and pass a parameter. 
+
+function Testimonials() {
+
+    return(
+        <div id="testimonials" className="Testimonials">
+            <div>
+                Testimonial 1
+                Photo, Name, Age (Optional)
+            </div>
+            <div>
+                Testimonial 2
+                Photo, Name, Age (Optional)
+            </div>
+        </div>
+    );
+}
+
+export default Testimonials
